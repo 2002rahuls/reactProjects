936e7a07382d88fdce9870ad1ee84c621b3f3a31
2002rahuls <rahulhs0212@gmail.com>
2024-02-13 23:26:25 +0530

    weather commit

---
+++ Weather_App/weatherApp/src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherApp from "./components/WeatherApp";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <WeatherApp></WeatherApp>;
+    </>
+  );
+}
+
+export default App;
